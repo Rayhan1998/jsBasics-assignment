89dfb2fed973faf4b0764078044c31fe41cd3a17
Rayhan1998 <miahrayhan01@gmail.com>
2021-10-16 21:53:32 -0400

    update

--- password.js
+++ password.js
@@ -8,26 +8,39 @@ const reader = readline.createInterface({
 console.log("Welcome to the password validator");
 
 reader.question("what is your password: ", function(input) {
-  let status = {
-    message: "",
-    error: false,
+  let passwordManager = {
+    state: {
+      message: "",
+      error: false
+    },
+
+    setPassword() {
+      this.state.error
+        ? console.log(this.state.message)
+        : console.log("success");
+    },
+
     checkPassword() {
-      this.error ? console.log(this.message) : console.log("success");
+      if (input.search(/[0-9]/) == -1) {
+        this.state.message = "your password needs a number";
+        this.state.error = true;
+      }
+
+      if (input.length < 10) {
+        this.state.message =
+          "failure your password needs to be atleast 10 charecters long";
+        this.state.error = true;
+      }
+    },
+
+    init() {
+      this.checkPassword();
     }
   };
-
-  if (input.search(/[0-9]/) == -1) {
-    status.message = "your password needs a number";
-    status.error = true;
-  }
-
-  if (input.length < 10) {
-    status.message =
-      "failure your password needs to be atleast 10 charecters long";
-    status.error = true;
-  }
-
-  status.checkPassword();
+  // initializes if statements
+  passwordManager.init();
+  // Sets password
+  passwordManager.setPassword();
 
   // This line closes the connection to the command line interface.
   reader.close();
